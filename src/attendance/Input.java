430325c98fb59716664e3108b7224a73725fa317
OBroin <@Furlong PC>
2016-12-02 19:46:26 -0600

    Adding more files I think

--- src/attendance/Input.java
+++ src/attendance/Input.java
@@ -28,7 +28,7 @@ public class Input extends javax.swing.JFrame {
     public Input() {
         initComponents();
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
